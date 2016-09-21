9e4689562c570f3dc476cbb6d4c1238020cea380
DYM <grunge.rock.seven@gmail.com>
2016-09-21 18:45:12 +0900

    latest commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'https://guarded-headland-13658.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_setting =
+  ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
