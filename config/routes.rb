455ab2755118f1213c3ee1acbf75b7383b3425aa
DYM <grunge.rock.seven@gmail.com>
2016-09-21 21:46:13 +0900

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-   devise_for :users, cotrollers:{
-     registrariions: "users/registrations",
-     omniauth_callbacks: "users/omniauth_callbacks"
+   devise_for :users, :controllers => {
+     :registrariions => "users/registrations",
+     :omniauth_callbacks => "users/omniauth_callbacks"
    }
    resources :posts
    root 'posts#index'
