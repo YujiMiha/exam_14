8b1ce6a90315495ca1fdf336125ef28c4f5afad1
DYM <grunge.rock.seven@gmail.com>
2016-09-22 20:43:23 +0900

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: {
-   registrations: "users/registrations" ,omniauth_callbacks: "users/omniauth_callbacks"}
-   }
-   resources :posts
+   devise_for :users, controllers: {
+    registrations: "users/registrations" ,omniauth_callbacks: "users/omniauth_callbacks"}
+  end
    root 'posts#index'
+   resources :posts
+
 
    if Rails.env.development?
      mount LetterOpenerWeb::Engine, at: "/letter_opnner"
