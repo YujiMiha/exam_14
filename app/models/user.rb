a567b4c74aef3d7ebfa9ecbae7330e0a6803f0be
DYM <grunge.rock.seven@gmail.com>
2016-09-22 21:20:30 +0900

    latest commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
           password: Devise.friendly_token[0, 20],
       )
       user.skip_confirmation!
-      user.save
+      user.save(validate: false)
     end
     user
   end
